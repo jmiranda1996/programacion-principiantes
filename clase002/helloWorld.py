a2463243e31f5c347581a3f1a3f8cbb3b99273b3
Jerson Miranda <jerson.miranda@outlook.es>
2020-11-22 17:18:52 -0500

    clase 002wq

---
+++ clase002/helloWorld.py
@@ -0,0 +1,9 @@
+import getch
+
+# Este programa imprime un Hola Mundo y 
+# espera que se presione una tecla para continuar
+
+print ('¡Hola Mundo!')
+print ('Presione una tecla para continuar...')
+
+getch.getch()
